455b4a36cf2835020298888d0c8a2944b9a93a61
jose zambrano <joseyzambranovpe@gmail.com>
2025-12-24 00:17:15 +0100

    Initial commit

---
+++ test/file-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FileService from '../lib/file-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/file-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FileService.FileServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
